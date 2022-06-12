b7b9bdca9835525fad484cf801f5e8ea8401975a
Tonight or never <vothuykg123@gmail.com>
2022-06-13 04:39:33 +0700

    62 - backup followers

--- BigSchool_ThaiTrungKien_1911061800/Controllers/AttendancesController.cs
+++ BigSchool_ThaiTrungKien_1911061800/Controllers/AttendancesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using BigSchool_ThaiTrungKien_1911061800.DTOs;
 using BigSchool_ThaiTrungKien_1911061800.Models;
 using Microsoft.AspNet.Identity;
 
@@ -20,14 +21,14 @@ namespace BigSchool_ThaiTrungKien_1911061800.Controllers
         }
 
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId)
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
         {
             var userId = User.Identity.GetUserId();
-            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == courseId))
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
                 return BadRequest("the attendance already exists");
             var attendance = new Attendance
             {
-                CourseId = courseId,
+                CourseId = attendanceDto.CourseId,
                 AttendeeId = userId
             };
             _dbContext.Attendances.Add(attendance);
